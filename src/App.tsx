061e4547995479f80e7b2c14e854d116f45421f7
Alexander van Trijffel <alexander@structura.ws>
2021-06-23 22:59:01 +0200

    feat: add image gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ImageGallery from "react-image-gallery";
 
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+  {
+    original:
+      "http://localhost:2342/api/v1/t/5b3c5b2232d546231205834496ae7f2a567037c2/45c097e5/tile_500",
+    thumbnail:
+      "http://localhost:2342/api/v1/t/5b3c5b2232d546231205834496ae7f2a567037c2/45c097e5/fit_2048",
+  },
+  {
+    original:
+      "http://localhost:2342/api/v1/t/a59b5326a1726eeefeedec8ad33c9cd4787de492/45c097e5/fit_2048",
+    thumbnail:
+      "http://localhost:2342/api/v1/t/a59b5326a1726eeefeedec8ad33c9cd4787de492/45c097e5/tile_500",
+  },
+];
+
+class MyGallery extends React.Component {
+  render() {
+    return <ImageGallery items={images} />;
+  }
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MyGallery />;
 }
 
 export default App;
