dcf0807fc62550f43fcb3feeeb89810c1f69680c
Alexander van Trijffel <alexander@structura.ws>
2023-05-09 23:28:18 +0200

    feat: convert to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </React.StrictMode>,
-  document.getElementById('root'),
 )
