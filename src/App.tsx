382e4f479d9448115bc4842ce1c5750170be6f9d
Alexander van Trijffel <alexander@structura.ws>
2023-06-29 00:19:05 +0200

    chore: test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import { AppApolloProvider } from './apolloClient'
 import PhotoGallery from './PhotoManager/PhotoGallery'
 
+console.log('app started...')
+
 function App() {
   return (
     <AppApolloProvider>
